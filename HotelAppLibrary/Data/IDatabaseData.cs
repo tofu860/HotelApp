01f2a1f737ab281427f82c7f3409616ce4c422bf
xyotech20@gmail.com <xyotech20@gmail.com>
2022-04-12 23:57:29 -0500

    Add project files.

---
+++ HotelAppLibrary/Data/IDatabaseData.cs
@@ -0,0 +1,26 @@
+﻿using HotelAppLibrary.Models;
+using System;
+using System.Collections.Generic;
+
+namespace HotelAppLibrary.Data
+{
+    public interface IDatabaseData
+    {
+        void BookGuest(string firstName,
+                      string lastName,
+                      string StreetAddress,
+                      string City,
+                      string ZipCode,
+                      string State,
+                      string PhoneNumber,
+                      string EmailAddress,
+                      DateTime startDate,
+                      DateTime endDate,
+                      int roomTypeId);
+
+void CheckInGuest(int bookingId);
+        List<RoomTypeModel> GetAvailableRoomTypes(DateTime startDate, DateTime endDate);
+        RoomTypeModel GetRoomTypeById(int id);
+        List<BookingFullModel> SearchBookings(string lastName);
+    }
+}
---
+++ HotelAppLibrary/Models/GuestModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelAppLibrary.Models
+{
+    public class GuestModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string EmailAddress { get; set; }
+
+
+    }
+}
